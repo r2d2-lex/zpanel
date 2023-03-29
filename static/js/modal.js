65b4ef8e0da7409905f4f6d0ca4f2e0e1105f4a7
Gogs <r2d2-lex@yandex.ru>
2023-03-29 08:37:54 +0500

    Отображение ошибок в модальном окне (в процессе)

---
+++ static/js/modal.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('a[data-bs-toggle=modal], button[data-toggle=modal]').click(function () {
+    let data_id = '';
+    if (typeof $(this).data('id') !== 'undefined') {
+      data_id = $(this).data('id');
+    }
+    $('.modal-body #host_id').val(data_id);
+    monitoring = 0;
+    console.log(monitoring);
+    })
+});
