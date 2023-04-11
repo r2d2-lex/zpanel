58c78a9e1797e969fdfec13c46a5a996b61136b2
Gogs <r2d2-lex@yandex.ru>
2023-04-11 11:13:33 +0500

    Таблица мониторинга параметров + не забыть ключ --autogenerate к alembic revision

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -9,3 +9,12 @@ class Host(Base):
     hostid = Column(Integer, unique=True, primary_key=True, index=True)
     column = Column(Integer)
     image = Column(String)
+
+
+class MonitoredItem(Base):
+    __tablename__ = 'item'
+    id = Column(Integer, unique=True, index=True, autoincrement=True, nullable=False)
+    hostid = Column(Integer, ForeignKey(Host.hostid, ondelete="CASCADE", onupdate="CASCADE"), nullable=False,
+                    primary_key=True)
+    name = Column(String)
+    value_type = Column(String)
