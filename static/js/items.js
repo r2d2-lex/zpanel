8544de71492e270fb539eafb00769562440b3d6c
Gogs <r2d2-lex@yandex.ru>
2023-04-13 15:41:43 +0500

    Добавление элемента + перезагрузка items

--- static/js/items.js
+++ static/js/items.js
@@ -5,17 +5,59 @@ $(items).filter(function() {
           $('#items_status').empty();
 
           let button_id = $(this).attr('id');
-          console.log(button_id);
-
           let item_template = 'item_';
           let host_id = button_id.replace(item_template,'');
-          console.log(host_id);
 
           $('#itemsModal').modal('show');
           // Устанавливаем значение input в модальной форме
           $("#itemsModal").find('input[name="items_id"]').val(host_id);
+          $("#itemsModal").find('h5').empty();
+          $("#itemsModal").find('h5').text('Данные для:' + host_id);
+          reloadItems(host_id);
+    });
+});
 
-          $.ajax({
+// Modal button click!!!
+$(document).on("click", "#item_add", function(event){
+    console.log('------------ NEW!!!! -------------------');
+    let host_id = $('#itemsModal').find('input[name="items_id"]').val();
+    let item_name = $('#item_name').val();
+    let item_type = $('#item_type').val();
+
+    console.log('Host id: ' + host_id + ' Item_Name: ' +  item_name + ' Item_Type: ' + item_type);
+    $.ajax({
+        type : 'POST',
+        url: '/items/',
+        data: JSON.stringify({
+            'host_id': host_id,
+            'name': item_name,
+            'value_type': item_type,
+        }),
+        dataType: 'json',
+        contentType: "application/json",
+        success: function(data){
+            showMessage('#items_status', 'Успешная добавлено: '+ host_id, 'alert-success');
+            console.log('success');
+            reloadItems(host_id);
+        },
+        error: function(data){
+            showMessage('#items_status', 'Неудачная операция: '+ host_id + ' Name: ' + item_name, 'alert-warning');
+            console.log('error');
+        }
+    });
+
+});
+
+$(document).on("click", "#item_path", function(event){
+    showMessage('#items_status', 'test!', 'alert-info');
+});
+
+$(document).on("click", "#item_delete", function(event){
+    showMessage('#items_status', 'test!', 'alert-danger');
+});
+
+function reloadItems(host_id) {
+            $.ajax({
             type : 'POST',
             url: '/data-items/',
             data: JSON.stringify({
@@ -25,24 +67,16 @@ $(items).filter(function() {
             contentType: "application/json",
             success: function (html) {
                 $("#items_content").html(html);
-                console.log('Success!');
+                console.log('Success load items!');
             },
             error: function(html){
-                console.log('Error!');
+                console.log('Error load items!');
             }
           });
+}
 
-    });
-
-});
-
-// Modal button click!!!
-$(document).on("click", "#item_add", function(event){
-    console.log('------------ NEW!!!! -------------------');
-//    let val2 = $('.modal-body input[name=items_id]').val();
-    let host_id = $('#itemsModal').find('input[name="items_id"]').val();
-    let item_name = $('#item_name').val();
-    let item_type = $('#item_type').val();
-
-    console.log('Host id: ' + host_id + ' Item_Name: ' +  item_name + ' Item_Type: ' + item_type);
-});
+function addSelectRecord(item_name, item_type) {
+    $('#select_items').append($('<option>', { value: item_name,
+        text : 'Элемент: ' + item_name + '; Тип данных:' + item_type,
+    }));
+}
