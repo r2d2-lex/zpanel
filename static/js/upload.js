a271cce1da6657047505e290c5bb6b617dde1861
Gogs <r2d2-lex@yandex.ru>
2023-04-28 09:16:22 +0500

    fix изменение изображения элемента без перезагруки страницы

--- static/js/upload.js
+++ static/js/upload.js
@@ -2,6 +2,9 @@ var img_items = $(".img_class");
 $(img_items).filter(function() {
     const $that = $(this);
     $that.click(function(){
+          $("#upload_id").show();
+          $('#result_upload').empty();
+
           const button_id = $that.attr('id');
           const img_template = 'img_';
           const host_id = button_id.replace(img_template,'');
@@ -20,8 +23,13 @@ $("#upload_id").change(function(){
 		var formData = new FormData();
 		formData.append('image', $("#upload_id")[0].files[0]);
 
-        const data_id = $('#Host-Id').val();
-        formData.append('host-id', data_id);
+        const host_id = $('#Host-Id').val();
+        formData.append('host-id', host_id);
+
+        const img_id = 'img_src_' + host_id;
+        const page_url = window.location.href;
+        const img_location = 'static/images/';
+        const img_path = page_url + img_location;
 
 		$.ajax({
 			type: "POST",
@@ -32,11 +40,12 @@ $("#upload_id").change(function(){
 			data: formData,
 			dataType : 'json',
 			success: function(msg){
-			    console.log(msg);
 				if (msg.error == '') {
 					$("#upload_id").hide();
 					showMessage('#result_upload', 'Успешная сохранено: '+msg.success, 'alert-success');
-					console.log('success: upload',msg.success)
+					console.log('success: upload ', msg.success);
+					// Меняем изображение элемента без перезагрузки страницы
+					$('#' + img_id).attr('src', img_path + msg.success);
 				} else {
 					showMessage('#result_upload', 'Неудачная операция: '+msg.error, 'alert-warning');
 					console.log('error: upload',msg.error)
@@ -45,7 +54,3 @@ $("#upload_id").change(function(){
 		});
 	}
 });
-
-$('#uploadModal').on('hide.bs.modal', function (e) {
-  show();
-})
