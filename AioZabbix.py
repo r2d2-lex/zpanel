0114c6d073edf5a3f0495f7e0abad0ef69668941
Gogs <r2d2-lex@yandex.ru>
2023-05-22 17:14:37 +0500

    get_host_problem+

--- AioZabbix.py
+++ AioZabbix.py
@@ -1,8 +1,9 @@
 import asyncio
 import config
+import datetime
 import json
 import logging
-
+from Zabbix import RECENT_PROBLEMS, SEVERITIES, PROBLEMS_OUTPUT_FIELDS_DICT, CLOCK_FIELD, TIME_TEMPLATE, SEVERITY_FIELD
 from aiorequest import post_data
 
 logging.basicConfig(level=config.LOGGING_LEVEL)
@@ -52,6 +53,10 @@ class AioZabbixApi:
         method = 'host.get'
         return await self._zabbix_request(method, params=params)
 
+    async def zabbix_problem_get(self, params):
+        method = 'problem.get'
+        return await self._zabbix_request(method, params=params)
+
     async def zabbix_login(self):
         method = 'user.login'
         params = {'user': config.ZABBIX_API_USER, 'password': config.ZABBIX_API_PASSWORD}
@@ -61,6 +66,15 @@ class AioZabbixApi:
     async def zabbix_logout(self):
         method = 'user.logout'
         return await self._zabbix_request(method)
+
+    async def get_host_problem(self, host_id) -> list:
+        return await self.zabbix_problem_get(dict(
+            hostids=host_id,
+            recent=RECENT_PROBLEMS,
+            severities=SEVERITIES,
+            output=PROBLEMS_OUTPUT_FIELDS_DICT,
+        )
+        )
 
     async def get_monitored_hosts(self, host_ids: list) -> list:
         return await self.zabbix_host_get(dict(
@@ -84,20 +98,45 @@ async def get_zabbix_monitoring_hosts(host_ids: list) -> list:
     return hosts
 
 
-async def main_async():
-    aio_zabbix = AioZabbixApi()
-    auth = await aio_zabbix.zabbix_login()
-    params = {'status': 1, 'monitored_hosts': 1, 'selectInterfaces': ['ip'], }
-    hosts = await aio_zabbix.zabbix_host_get(params)
-    exit = await aio_zabbix.zabbix_logout()
-    print(f'{auth} {hosts} {exit}')
+async def get_all_zabbix_monitoring_hosts() -> list:
+    async with AioZabbixApi() as aio_zabbix:
+        params = dict(status=1, monitored_hosts=1, selectInterfaces=['ip'])
+        hosts = await aio_zabbix.zabbix_host_get(params)
+    return hosts
+
+
+async def get_async_host_problem(api, host_id: int, all_results: list):
+    problem_list = []
+    host_problems = await api.get_host_problem(host_id)
+
+    for problem in host_problems:
+        try:
+            clock = datetime.datetime.fromtimestamp(int(problem[CLOCK_FIELD]))
+            clock = clock.strftime(TIME_TEMPLATE)
+            problem.update({CLOCK_FIELD: clock})
+            problem_list.append(problem)
+        except KeyError as error:
+            logging.error(f'KeyError: {error}')
+            continue
+
+    all_results.append(dict(host_id=host_id, problems=problem_list))
+
+
+async def get_host_details(host_ids: list) -> list:
+    all_results = []
+    async with AioZabbixApi() as aio_zabbix:
+        futures = [asyncio.ensure_future(get_async_host_problem(aio_zabbix, id, all_results)) for id in host_ids]
+        await asyncio.wait(futures)
+    return all_results
 
 
 def main():
     loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(get_zabbix_monitoring_hosts([10454, 10462]))
+    # result = loop.run_until_complete(get_zabbix_monitoring_hosts([10454, 10462]))
+    result = loop.run_until_complete(get_host_details([10472, 10434]))
     loop.close()
-    print(result)
+    for item in result:
+        print(item)
 
 
 if __name__ == '__main__':
