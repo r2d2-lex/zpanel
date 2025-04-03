59e685ccba4440e11a9adf17fd2995da011d64e3
Gogs <r2d2-lex@yandex.ru>
2025-04-03 16:27:36 +0500

    get_auth_user_username проверка на правильную аутентификацию

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,8 @@
+import secrets
 from typing import  Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import config
 
 router = APIRouter(prefix='/auth', tags=['Auth'])
 
@@ -13,4 +15,37 @@ def basic_auth(
     return {
         'username': credentials.username,
         'password': credentials.password,
+    }
+
+
+def get_auth_user_username(
+        credentials: Annotated[HTTPBasicCredentials, Depends(security) ]
+) -> str:
+    unauthed_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Неправильный логин или пароль',
+        headers={'WWW-Authenticate':'Basic'},
+    )
+    if credentials.username not in config.ZPANEL_SETTINGS_LOGIN:
+        raise unauthed_exception
+
+    correct_password = config.ZPANEL_SETTINGS_PASSWORD
+    if correct_password is None:
+        raise unauthed_exception
+
+    if not secrets.compare_digest(
+            credentials.password.encode('utf-8'),
+            correct_password.encode('utf-8'),
+    ):
+        raise unauthed_exception
+
+    return credentials.username
+
+@router.get('/username')
+def basic_auth_username(
+        auth_username: str = Depends(get_auth_user_username)
+):
+    return {
+        'message': f'Hi, {auth_username}',
+        'username': auth_username,
     }
