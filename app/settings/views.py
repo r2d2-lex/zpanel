6a549f51820dadc43d8738cfa2327da3db149b72
Gogs <r2d2-lex@yandex.ru>
2025-04-15 10:55:53 +0500

    Спрашиваем логин и пароль при входе в настройки

--- app/settings/views.py
+++ app/settings/views.py
@@ -4,9 +4,11 @@ from fastapi.responses import HTMLResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import logging
-logger = logging.getLogger(__name__)
-
 import time
+
+from auth.views import get_auth_user_username
+
+logger = logging.getLogger(__name__)
 
 
 from common import templates
@@ -17,12 +19,15 @@ from service import get_host_details
 router = APIRouter(tags=['settings'])
 
 @router.get('/settings', response_class=HTMLResponse)
-async def ajax_settings(request: Request, db: AsyncSession = Depends(get_db)):
+async def ajax_settings(request: Request,
+                        db: AsyncSession = Depends(get_db),
+                        auth_username: str = Depends(get_auth_user_username),
+                        ):
     time_start = time.time()
     template = 'zpanel/settings.html'
     zabbix_hosts = await get_all_zabbix_monitoring_hosts()
     monitoring_hosts = await get_host_details(zabbix_hosts, db)
-    logger.info(f'Function SETTINGS delta time: {time.time() - time_start}')
+    logger.info(f'Function SETTINGS delta time: {time.time() - time_start}. Username: \'{auth_username}\'')
     return templates.TemplateResponse(template,
                                       {
                                           'request': request,
@@ -31,11 +36,14 @@ async def ajax_settings(request: Request, db: AsyncSession = Depends(get_db)):
                                       )
 
 @router.get('/', response_class=HTMLResponse)
-def index(request: Request):
+def index(request: Request,
+          auth_username: str = Depends(get_auth_user_username),
+):
     return templates.TemplateResponse('zpanel/index.html',
                                       {
                                           'request': request,
                                           'page_title': 'Настройка',
+                                          'username': auth_username,
                                       }
                                       )
 
