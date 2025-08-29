d104a6ca932032a101934442db9540883f2debf9
Gogs <r2d2-lex@yandex.ru>
2025-08-29 13:47:24 +0500

    показываем название исключения

--- app/exceptions.py
+++ app/exceptions.py
@@ -2,4 +2,9 @@ import aiohttp
 
 
 class BadRequestFromApi(aiohttp.ClientError):
-    pass
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {self.message}"
