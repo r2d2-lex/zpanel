e3bc037c12d6fc6101be789acf5d14914daa0352
Gogs <r2d2-lex@yandex.ru>
2025-09-05 13:00:34 +0500

    fix теста test_index - мок для get_auth_user_username

--- tests/settings/test_settings_views.py
+++ tests/settings/test_settings_views.py
@@ -1,13 +1,22 @@
 from unittest.mock import patch, AsyncMock
 from models import MonitoredItem
 from items.views import get_item_by_host_id
+from auth.views import get_auth_user_username
 from conftest import app
+
+TEST_USERNAME = 'test'
+
+def mock_get_auth_user_username():
+    return TEST_USERNAME
+
+app.dependency_overrides.update({get_auth_user_username: mock_get_auth_user_username})
 
 async def test_index(client):
     response = await client.get('/')
     assert response.status_code == 200
     assert 'js/index.js' in response.text
     assert 'Настройка' in response.text
+    assert TEST_USERNAME in response.text
 
 async def test_ajax_settings(client):
     with patch('settings.views.get_all_zabbix_monitoring_hosts', new_callable=AsyncMock) as mock_get_all_zabbix_mh, \
@@ -16,10 +25,11 @@ async def test_ajax_settings(client):
             {'host':'node1011', 'hostid':'99632', 'name':'node1011'},
             {'host':'node1012', 'hostid':'99633', 'name':'node1012'},
         ]
-        mock_get_hosts_details.return_value =  [
+        mock_get_hosts_details.return_value =  ([
             {'host':'node1011', 'hostid':'99632', 'name':'node1', 'column': 1},
             {'host':'node1012', 'hostid':'99633', 'name':'node2', 'column': 2},
-        ]
+        ],  [] # Host details problem was here
+        )
         response = await client.get('/settings')
         assert response.status_code == 200
         assert 'node1011' in response.text
